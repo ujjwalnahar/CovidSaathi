4c4fd242f91081f13abd65e6eba275aa86720689
mayur <mayur25jan@gmail.com>
2021-02-03 15:12:42 +0530

    Initial Commit 0.01

---
+++ app/src/main/java/com/projectupma/activities/ApprovalActivity.java
@@ -0,0 +1,16 @@
+package com.projectupma.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projectupma.R;
+
+public class ApprovalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_approval);
+    }
+}
